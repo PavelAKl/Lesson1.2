79c1835fc4c29aaa453eb0a4c8c68bc33a1bcc2c
PavelAKl <pavelandreevichk98@gmail.com>
2024-12-14 18:06:52 +0300

    Add files via upload

---
+++ peremen.py
@@ -0,0 +1,10 @@
+kol_vip_dz = 12
+kol_zat_ch = 1.5
+naz_kyr = 'Python'
+vre_na_od_zad = kol_zat_ch/kol_vip_dz
+print('Курс:',naz_kyr,', всего задач:',kol_vip_dz,', затрачено часов:',kol_zat_ch,', среднее время выполнения:',vre_na_od_zad)
+a = 12
+b = 1.5
+c = 'Python'
+d = b/a
+print('kyrs:', c, 'vsego zadach:', a,'zatrach chasov:', b, 'sred vremy:',d)
